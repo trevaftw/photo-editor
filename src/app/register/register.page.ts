c6fe670194d82384b001a96773b95050671170df
Trevor Ramlow <trevorramlow@gmail.com>
2020-03-04 16:49:07 -0600

    added alert for registration error and routing for after logging in. next need to build out tabs and pages for post login session

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, } from '@angular/core';
+import { Router } from '@angular/router'
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -9,27 +11,44 @@ import { auth } from 'firebase/app';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-//initialize the variables
-username: string = "";
-password: string = "";
-confirmPassword: string = "";
+  //initialize the variables
+  username: string = "";
+  password: string = "";
+  confirmPassword: string = "";
 
-constructor(public afAuth: AngularFireAuth) { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public alert: AlertController,
+    public router: Router,
+  ) { }
 
-ngOnInit() {
-}
-
-async register() {
-  const { username, password, confirmPassword } = this;
-  if(password !== confirmPassword){
-    alert("Passwords do not match.")
+  ngOnInit() {
   }
-  try {
-    const result = await this.afAuth.auth.createUserWithEmailAndPassword(username, password);
-    console.log('reg result:', result)
-  } catch (error) {
-    console.log('Error with registration:', error)
+
+  async register() {
+    const { username, password, confirmPassword } = this;
+    if (password !== confirmPassword) {
+      this.showAlert("Error", "Passwords do not match")
+    }
+    try {
+      const result = await this.afAuth.auth.createUserWithEmailAndPassword(username, password);
+      console.log('reg result:', result)
+      //eventually make this alert into a toast instead of an alerty
+      this.showAlert("Success!", "Welcome aboard")
+      this.router.navigate(['/tabs'])
+    } catch (error) {
+      this.showAlert('Error with registration:', error)
+    }
   }
-}
+
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ["Ok"]
+    })
+    await alert.present()
+  }
 
 }
